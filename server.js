d5bb72dfc1bd6fafa0c74cd58068be04230b5ede
browe83 <rowe.brandon.j@gmail.com>
2020-10-10 16:16:01 -0400

    adds get and post requests

--- server.js
+++ server.js
@@ -13,6 +13,31 @@ app.get("/api/quotes/random", (req, res, next) => {
   res.send({ quote: randQuoteObj });
 });
 
+app.get("/api/quotes", (req, res, next) => {
+  const author = req.query.person;
+  if (author !== undefined) {
+    const quotesByAuthor = quotes.filter((quote) => quote.person === author);
+    res.send({
+      quotes: quotesByAuthor,
+    });
+  } else {
+    res.send({ quotes: quotes });
+  }
+});
+
+app.post("/api/quotes", (req, res, next) => {
+  if (req.query.quote && req.query.person) {
+    const newQuote = {
+      quote: req.query.quote,
+      person: req.query.person,
+    };
+    quotes.push(newQuote);
+    res.send({ quote: newQuote });
+  } else {
+    res.status(400).send("Missing information. Quote could not be created.");
+  }
+});
+
 app.listen(PORT, () => {
   console.log(`Listening at port: ${PORT}`);
 });
