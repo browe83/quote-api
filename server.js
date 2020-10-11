d3873b7a19d8222929f1c7b697441d804f8b5e2a
browe83 <rowe.brandon.j@gmail.com>
2020-10-10 21:03:42 -0400

    debugs project

--- server.js
+++ server.js
@@ -13,6 +13,36 @@ app.get("/api/quotes/random", (req, res, next) => {
   res.send({ quote: randQuoteObj });
 });
 
+app.get("/api/quotes", (req, res, next) => {
+  if (req.query.person) {
+    const author = req.query.person;
+    const quotesByAuthor = quotes.filter((quote) => author === quote.person);
+    res.send({
+      quotes: quotesByAuthor,
+    });
+  } else {
+    res.send({
+      quotes: quotes,
+    });
+  }
+});
+
+app.post("/api/quotes", (req, res, next) => {
+  const person = req.query.person;
+  const quote = req.query.quote;
+
+  console.log(req.query);
+  if (!person || !quote) {
+    res.status(400).send({ message: "something went wrong" });
+  } else {
+    const newQuoteObj = {
+      quote: quote,
+      person: person,
+    };
+    quotes.push(newQuoteObj);
+    res.send({ quote: { person, quote } });
+  }
+});
 app.listen(PORT, () => {
   console.log(`Listening at port: ${PORT}`);
 });
